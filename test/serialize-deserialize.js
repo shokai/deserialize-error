b96c4a3b2b7a2c1ef17492cff02521f455127d5d
Sho Hashimoto <hashimoto@shokai.org>
2016-09-10 17:36:44 +0900

    add tests

--- test/serialize-deserialize.js
+++ test/serialize-deserialize.js
@@ -15,4 +15,37 @@ describe('serialize-deserialize', function () {
       assert.equal(err.toString(), err2.toString())
     })
   })
+
+  describe('custom Error object', function () {
+    class FooError extends Error {
+      constructor(message){
+        super(message);
+        this.name = 'FooError';
+      }
+    }
+    let err = new FooError('barrrrrr')
+    let err2 = deserializeError(serializeError(err))
+    it('should be equal', function () {
+      assert.equal(err.name, err2.name)
+      assert.equal(err.message, err2.message)
+      assert.equal(err.stack, err2.stack)
+      assert.equal(err.toString(), err2.toString())
+    })
+  })
+
+  describe('string', function () {
+    let err
+    try {
+      throw 'fooooo'
+    } catch (_err) {
+      err = _err
+    }
+    let err2 = deserializeError(serializeError(err))
+    it('should be equal', function () {
+      assert.equal(err.name, err2.name)
+      assert.equal(err.message, err2.message)
+      assert.equal(err.stack, err2.stack)
+      assert.equal(err.toString(), err2.toString())
+    })
+  })
 })
